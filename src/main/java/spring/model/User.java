153020f1cb120cf7c0b54357e474b716c51319b0
Partizanin <topiciv1@ayndex.ua>
2018-04-01 15:01:49 +0200

    add new table

--- src/main/java/spring/model/User.java
+++ src/main/java/spring/model/User.java
@@ -6,7 +6,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "empdata", catalog = "employees")
+@Table(name = "users", catalog = "employees")
 public class User {
 
     @Id
--- src/main/java/spring/repository/UsersRepository.java
+++ src/main/java/spring/repository/UsersRepository.java
@@ -7,6 +7,5 @@ import java.util.List;
 
 public interface UsersRepository extends JpaRepository<User, Integer> {
     List<User> findByName(String name);
-
     User findById(int id);
 }
