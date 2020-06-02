81deb8ea60e7b0053b3d996c23d69b77ee17d72a
Partizanin <topiciv1@yandex.ua>
2020-06-02 19:43:39 +0300

    revert to 84cca3075502bf2eb2936e083323e20656d2c148

--- src/main/kotlin/example/spring/repository/EmployeeRepository.kt
+++
@@ -1,7 +0,0 @@
-package example.spring.repository
-
-import example.spring.model.Employee
-import org.springframework.data.jpa.repository.JpaRepository
-
-
-interface EmployeeRepository : JpaRepository<Employee, Long>
